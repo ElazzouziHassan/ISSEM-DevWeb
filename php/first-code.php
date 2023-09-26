8d14de683c76a045be1008e86dcaffc94da1a191
wizardy <wizardhassan99@gmail.com>
2023-09-26 14:44:20 +0100

    PHP : hello world mixed in HTML

--- php/first-code.php
+++ php/first-code.php
@@ -1,1 +1,11 @@
-<?php echo "Hello World!"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP | Tutorial</title>
+</head>
+<body>
+  <?php echo "<h1 align='center'>Hello, World!</h1>" ?>
+</body>
+</html>

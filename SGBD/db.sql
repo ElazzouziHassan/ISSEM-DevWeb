49918214f0f2368ed7089665315ee438b3e7f772
wizardy <wizardhassan99@gmail.com>
2023-09-28 20:39:39 +0100

    commit

--- SGBD/db.sql
+++ SGBD/db.sql
@@ -1,8 +1,15 @@
 -- Active: 1678706437666@@127.0.0.1@3306@test_db
+-- this commande to create a database :
 CREATE DATABASE test_db;
-
+-- to use it try use 'USE' commande followed by the name of the database :
 use test_db;
 
+-- SQL Syntax to create a table :
+/*
+* 1| CREATE TABLE `TABLE_NAME`(
+* 2|   colloumnName data_type constraints,
+* 3| )
+*/
 -- table adherent :
 create table `adherent`(
   id_adh INT PRIMARY key AUTO_INCREMENT COMMENT 'Primary Key',
@@ -11,7 +18,6 @@ create table `adherent`(
   date_naissance DATE,
   create_time DATETIME COMMENT 'Create Time'
 )
-
 -- table responsable :
 CREATE Table `responsable`(
   id_resp INT PRIMARY KEY AUTO_INCREMENT,
@@ -19,7 +25,6 @@ CREATE Table `responsable`(
   prenom VARCHAR(50) NOT NULL,
   create_time DATETIME COMMENT 'Create Time'
 )
-
 -- table activite :
 CREATE Table `activite`(
   id_act INT PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
